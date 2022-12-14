589d84876bc794109d769572f2a1fb1f61931d21
rajat suthar <36098289+rajatsuthar@users.noreply.github.com>
2022-12-14 15:53:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+
+        <Switch>
+          <Route path="/search">
+            <h1>this is a search page</h1>
+          </Route>
+          <Route path="/">
+            <Home/>
+            </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
